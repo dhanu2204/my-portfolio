3128690eb4960926860832899acd25aacfcc8ce6
Dhanush <mmdhanush2204@gmail.com>
2026-06-08 21:33:07 +0530

    fix: replace deprecated tsparticles packages to fix vercel build

--- frontend/src/components/ParticlesBackground.jsx
+++ frontend/src/components/ParticlesBackground.jsx
@@ -1,23 +1,23 @@
-import React, { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesBackground = () => {
-  const particlesInit = useCallback(async (engine) => {
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    await loadFull(engine);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  const particlesLoaded = useCallback(async (container) => {
-    // console.log(container);
-  }, []);
+  if (!init) return <></>;
 
   return (
     <Particles
       id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
       options={{
         background: {
           color: {
